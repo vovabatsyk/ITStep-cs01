f2d11fa26a6b8c3efd32a7a8d95cecba18c92f0a
Volodymyr Batsyk <vovabatsyk@gmail.com>
2020-09-14 21:03:23 +0300

    Home Work 2 (Array, String)

---
+++ folder_02/Array.cs
@@ -0,0 +1,215 @@
+﻿using System;
+
+namespace Array
+{
+    class Array
+    {
+        static void Main(string[] args)
+        {
+
+            int[] a = { 10, 5, 4, 6, 4, 6, 5, 5, 4, 8, 9 };
+            Console.WriteLine("Дан одномерный массив А, состоящий из N элементов. Сколько значений элементов в массиве А встречается более одного раза ? ");
+            PrintArray(a);
+            Console.WriteLine($"Результат: {FindMoreThanOne(a)}");
+            Console.WriteLine();
+
+            Console.WriteLine("Дан одномерный массив А, состоящий из N элементов. Сколько значений элементов встречается в массиве по 3 раза ? ");
+            PrintArray(a);
+            Console.WriteLine($"Результат: {FindEqual(a, 3)}");
+            Console.WriteLine();
+
+            Console.WriteLine("Дан одномерный массив А, состоящий из N элементов. Определить количество чисел, входящих в массив по одному разу.");
+            PrintArray(a);
+            Console.WriteLine($"Результат: {FindEqual(a, 1)}");
+            Console.WriteLine();
+
+            Console.WriteLine("Дан одномерный массив А, состоящий из N элементов. Подсчитать максимальное количество подряд идущих нулей.");
+            int[] a2 = { 0, 0, -10, 5, -1, 0, 0, 0, 6, -5, 8, 9, 0, 0 };
+            PrintArray(a2);
+            Console.WriteLine($"Результат: {FindZero(a2)}");
+            Console.WriteLine();
+
+            Console.WriteLine("Дан одномерный массив А, состоящий из N элементов. Перенести в начало массива все положительные элементы, а в конец массива - все отрицательные.");
+            PrintArray(a2);
+            NegativePlace(a2);
+            PrintArray(a2);
+            Console.WriteLine();
+
+            Console.WriteLine("Дан одномерный массив А, состоящий из N элементов. Перенести в начало массива все четные элементы, а в конец массива - все нечетные.");
+            PrintArray(a);
+            EvenPlace(a);
+            PrintArray(a);
+            Console.WriteLine();
+
+            Console.WriteLine("Дан одномерный массив А, состоящий из N элементов. Подсчитать максимальное количество подряд идущих отрицательных элементов.");
+            int[] a3 = { -2, -10, 5, -1, -4, -6, 0, 6, -5, 8, 9 };
+            PrintArray(a3);
+            Console.WriteLine($"Результат: {FindNegativeCount(a2)}");
+            Console.WriteLine();
+
+            Console.WriteLine("Дан одномерный массив А, состоящий из N элементов. Найти первый и последний положительные элементы массива и подсчитать количество элементов, заключенных между ними.");
+            int[] a4 = { 0, -2, 4, 2, 5, 3, 7, 9, 4, 0 };
+            PrintArray(a4);
+            Console.WriteLine($"Результат: {CountPositiveElements(a4)}");
+            Console.WriteLine();
+
+        }
+
+        static void PrintArray(int[] arr)
+        {
+            foreach (int a in arr)
+            {
+                Console.Write($"{a}\t");
+            }
+            Console.WriteLine();
+        }
+
+        static void SortArray(int[] a, int n)
+        {
+            int i, j, k;
+            for (i = 1; i < n; ++i)
+            {
+                k = a[i];
+                for (j = i - 1; (j >= 0) && (a[j] > k); --j)
+                    a[j + 1] = a[j];
+                a[j + 1] = k;
+            }
+        }
+
+        static int FindMoreThanOne(int[] a)
+        {
+
+            int i, j, k;
+            int n = a.Length;
+            SortArray(a, n);
+            for (k = i = 0; i < n; i = j)
+            {
+                for (j = i + 1; (j < n) && (a[j] == a[i]); ++j)
+                    ;
+                if ((j - i) > 1)
+                {
+                    ++k;
+                }
+            }
+
+            return k;
+        }
+
+        static int FindEqual(int[] a, int num)
+        {
+            int i, j, k;
+            int n = a.Length;
+            SortArray(a, n);
+            for (k = i = 0; i < n; i = j)
+            {
+                for (j = i + 1; (j < n) && (a[j] == a[i]); ++j)
+                    ;
+                if ((j - i) == num)
+                {
+                    ++k;
+                }
+            }
+
+            return k;
+        }
+
+        static int FindZero(int[] arr)
+        {
+            int max = 0, k = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == 0)
+                    k++;
+                else
+                    k = 0;
+                if (k > max)
+                    max = k;
+            }
+            return max;
+        }
+
+        static void NegativePlace(int[] arr)
+        {
+            int i, j, temp;
+            for (i = 0, j = arr.Length - 1; i < j;)
+            {
+
+                temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+                if (arr[i] >= 0)
+                {
+                    i += 1;
+
+                }
+                if (arr[j] < 0)
+                {
+                    j -= 1;
+                }
+            }
+        }
+
+        static void EvenPlace(int[] arr)
+        {
+            int i, j, temp;
+
+            for (i = 0, j = arr.Length - 1; i < j;)
+            {
+                if (arr[i] % 2 != 0 && arr[j] % 2 == 0)
+                {
+                    temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+                if (arr[i] % 2 == 0)
+                {
+                    i += 1;
+                }
+                if (arr[j] % 2 != 0)
+                {
+                    j -= 1;
+                }
+            }
+        }
+
+        static int FindNegativeCount(int[] arr)
+        {
+            int max = 0, k = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] < 0)
+                    k++;
+                else
+                    k = 0;
+                if (k > max)
+                    max = k;
+            }
+            return max;
+        }
+
+        static int CountPositiveElements(int[] arr)
+        {
+            int res = 0,
+                start = 0,
+                end = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+                if (arr[i] > 0)
+                {
+                    start = i;
+                    break;
+                }
+
+
+            for (int i = 0; i < arr.Length; i++)
+                if (arr[i] > 0)
+                    end = i;
+
+            for (int i = start; i < end - 1; i++)
+                ++res;
+
+            return res;
+        }
+
+    }
+}
---
+++ folder_02/String.cs
@@ -0,0 +1,244 @@
+﻿    using System;
+using System.Data;
+using System.Text;
+
+namespace String
+{
+    class String
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Создать метод принимающий, введенную пользователем, строку и выводящий на экран статистику по этой строке.Статистика должна включать общее количество символов, количество букв(и сколько в верхнем регистре и нижнем), количество цифр, количество символов пунктуации и количество пробельных символов. ");
+            Console.Write("Введите строку - ");
+            string str = Console.ReadLine();
+            StatisticsForString(str);
+            Console.WriteLine();
+
+            Console.WriteLine("Пользователь вводит строку и символ. В строке найти все вхождения этого символа и перевести его в верхний регистр, а также удалить часть строки, начиная с последнего вхождения этого символа и до конца. ");
+            Console.Write("Введите строку - ");
+            str = Console.ReadLine();
+            Console.Write("Введите символ - ");
+            char symb = Console.ReadLine()[0];
+            SearchSymbol(str, symb);
+            Console.WriteLine();
+
+            Console.WriteLine("Дана строка символов. Осуществить в этой строке поиск заданного слова.");
+            Console.Write("Введите строку - ");
+            str = Console.ReadLine();
+            Console.Write("Введите слово - ");
+            string word = Console.ReadLine();
+            Console.WriteLine(SearchWord(str, word));
+            Console.WriteLine();
+
+            Console.WriteLine("Дан текст. Осуществить в этом тексте поиск и замену заданной фразы.");
+            Console.Write("Введите текст - ");
+            string text = Console.ReadLine();
+            Console.Write("Введите что заменить - ");
+            str = Console.ReadLine();
+            Console.Write("Введите на что заменить - ");
+            word = Console.ReadLine();
+            ReplaceWord(text, str, word);
+            Console.WriteLine();
+
+            Console.WriteLine("Создать функцию для проверки корректности расстановки скобок в выражении.");
+            Console.Write("Введите строку - ");
+            str = Console.ReadLine();
+            Console.WriteLine(CheckBrackets(str));
+            Console.WriteLine();
+
+            Console.WriteLine("Пользователь вводит с клавиатуры арифметическое выражение. Необходимо вычислить его значение с учетом приоритетов введенных математических операций и скобок.Если в выражении встречаются другие символы, выдать сообщение, что выражение введено не корректно. 5*2-(3+5)=");
+            Console.Write("Введите строку - ");
+            try
+            {
+                str = Console.ReadLine();
+                var result = new DataTable().Compute(str.TrimEnd('='), null);
+                Console.WriteLine($"Результат = {result}");
+            }
+            catch (Exception)
+            {
+
+                Console.WriteLine("Не корректное значение");
+            }
+
+            Console.WriteLine("Написать функцию, преобразующую дробное или целое число в строку");
+            Console.Write("Введите число - ");
+            try
+            {
+                double num = double.Parse(Console.ReadLine());
+                Console.WriteLine($"Строка: {ParseToString(num)}");
+            }
+            catch (Exception)
+            {
+
+                Console.WriteLine("Не корректное значение"); ;
+            }
+            Console.WriteLine();
+
+
+            Console.WriteLine("Написать функцию, преобразующую строку в дробное или целое число.");
+            Console.Write("Введите строку - ");
+            str = Console.ReadLine();
+            ParseToNumber(str);
+            Console.WriteLine();
+
+            Console.WriteLine("Написать функцию, которая определяет является ли строка палиндромом");
+            str = "аргентина манит негра";
+            Console.WriteLine($"{str} - палиндром : {IsPolyndrome(str)}");
+            str = "А роза упала на лапу Азора";
+            Console.WriteLine($"{str} - палиндром : {IsPolyndrome(str)}");
+            Console.WriteLine();
+
+            Console.WriteLine("Сколько раз в строке, введенной пользователем, встречается указанное слово?");
+            Console.Write("Введите строку - ");
+            str = Console.ReadLine();
+            Console.Write("Введите слово - ");
+            word = Console.ReadLine();
+            Console.WriteLine($"Слово \"{word}\" встречается: {CountWord(str, word)} раз(а)");
+            Console.WriteLine();
+        }
+
+        static void StatisticsForString(string str)
+        {
+            int digit = 0, letter = 0, lower = 0, upper = 0, punctuation = 0, whiteSpace = 0;
+            str.ToCharArray();
+
+            Console.WriteLine($"количество символов:\t{str.Length}");
+
+            foreach (char ch in str)
+            {
+                if (Char.IsDigit(ch))
+                {
+                    ++digit;
+                }
+
+                if (Char.IsLetter(ch))
+                {
+                    ++letter;
+                    if (Char.IsLower(ch))
+                    {
+                        ++lower;
+                    }
+                    if (Char.IsUpper(ch))
+                    {
+                        ++upper;
+                    }
+                }
+                if (Char.IsPunctuation(ch))
+                {
+                    ++punctuation;
+                }
+                if (Char.IsWhiteSpace(ch))
+                {
+                    ++whiteSpace;
+                }
+            }
+            Console.WriteLine($"количество цифр:\t{digit}");
+            Console.WriteLine($"количество букв:\t{letter}");
+            Console.WriteLine($"в нижнем регистре:\t{lower}");
+            Console.WriteLine($"в верхнем регистре:\t{upper}");
+            Console.WriteLine($"символы пунктуации:\t{punctuation}");
+            Console.WriteLine($"символы пунктуации:\t{whiteSpace}");
+
+        }
+
+        static void SearchSymbol(string str, char symb)
+        {
+            int idx = 0;
+            foreach (char ch in str)
+            {
+                if (ch == symb || Char.ToUpper(symb) == ch)
+                {
+                    ++idx;
+                }
+            }
+            Console.WriteLine($"вхождения этого символа:\t{idx}");
+            Console.WriteLine($"удалено часть строки:\t{str.Substring(str.LastIndexOf(symb) + 1)}");
+
+        }
+
+        static bool SearchWord(string str, string word)
+        {
+            string[] words = str.Split(new char[] { ' ' });
+
+            foreach (string s in words)
+            {
+                if (s.Equals(word))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        static void ReplaceWord(string text, string str, string word)
+        {
+
+            Console.WriteLine(text.Replace(str, word));
+        }
+
+        static bool CheckBrackets(string str)
+        {
+            int cnt = 0;
+            foreach (char c in str)
+            {
+                if (c == '(')
+                    cnt++;
+                else if (c == ')')
+                    cnt--;
+                if (cnt < 0) return false;
+            }
+            return cnt == 0;
+        }
+
+        static string ParseToString( double num)
+        {
+
+            return num.ToString();
+        }
+
+        static void ParseToNumber(string str)
+        {
+            try
+            {
+                double.Parse(str);
+                Console.WriteLine($"число: {str}");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine($"Невозможно преобразовать {str} в число "); 
+                
+            }
+        }
+
+        static bool IsPolyndrome(string input)
+        {
+            string tmp = input.Replace(" ", "");
+            return tmp == Reverse(tmp);
+        }
+
+        static string Reverse(string input)
+        {
+           StringBuilder sb = new StringBuilder();
+            for (int i = input.Length - 1; i >= 0; i--)
+                sb.Append(input[i]);
+            return sb.ToString();
+        }
+        
+        static int CountWord(string str, string word)
+        {
+            int count = 0;
+            string[] words = str.Split(new char[] { ' ' });
+            foreach (string s in words)
+            {
+                if (s.Equals(word))
+                {
+                    ++count;
+                }
+            }
+
+            return count;
+        }
+
+  
+    }
+}
